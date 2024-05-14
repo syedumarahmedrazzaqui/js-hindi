7369e2447298e7fd78449f2ef8edfbfd8f1881ac
syedumarahmedrazzaqui <116761130+syedumarahmedrazzaqui@users.noreply.github.com>
2024-05-14 11:36:17 +0000

    json

---
+++ 02_basics/18_objects.js
@@ -0,0 +1,27 @@
+const course = {
+    coursename : "js-Hindi",
+    price : "999",
+    courseinstructor : "hitesh"
+}
+
+// course.courseinstructor
+
+const {courseinstructor : instructor} = course
+
+//object de-structure
+// console.log(courseinstructor);
+
+console.log(instructor);
+
+//this is known as json because object has const and var
+// {
+//     "name" : "zaid",
+//     "coursename" : "js course in hindi",
+//     "price" : "999"
+// }
+
+[
+    {},
+    {},
+    {}
+]

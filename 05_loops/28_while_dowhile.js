201c513b5b0bf09a05e10952d74b9868a4757af6
syedumarahmedrazzaqui <116761130+syedumarahmedrazzaqui@users.noreply.github.com>
2024-05-19 10:46:23 +0000

    while loop

---
+++ 05_loops/28_while_dowhile.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <= 10) {
+    console.log(`the value of index is ${index}`);
+    index = index +2
+}
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    // arr++
+    arr = arr + 1
+}
+
+let score = 1;
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 10);
